533abc33530660dfc14dce951674f1a86527a28b
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-02-13 02:10:43 -0800

    Adding ABC recipebase for recipes to extend off of, started on unpack recipe

---
+++ sprinter/recipebase.py
@@ -0,0 +1,27 @@
+"""
+Recipe base is an abstract base class outlining the method required
+and some documentation on what they should provide.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+class RecipeBase:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def setup(self, config):
+        """ Setup performs the setup required, with the config
+        specified """
+        pass
+
+    @abstractmethod
+    def update(self, config, old_config):
+        """ Setup performs the setup required, and works with the old
+        config is destruction is required """
+        pass
+
+    @abstractmethod
+    def destroy(self, old_config):
+        """ Destroys an old feature if it is no longer required """
+        pass
---
+++ sprinter/recipes/unpack.py
@@ -0,0 +1,17 @@
+"""
+Unpacks and deploys a location
+"""
+
+from sprinter.recipebase import RecipeBase
+
+
+class Recipe(RecipeBase):
+
+    def setup(self, config):
+        pass
+
+    def update(self, config):
+        pass
+
+    def destroy(self, config):
+        pass
