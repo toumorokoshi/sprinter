a3140714ac81ec2061dbc0bc5ee77e59201f2849
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-02-14 22:35:42 -0800

    adding basic setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sprinter',
+      version='1.0',
+      description='a utility library to help environment bootstrapping scripts',
+      author='Yusuke Tsutsumi',
+      author_email='yusuke@yusuketsutsumi.com',
+      packages=['sprinter'],
+     )
