f2a75063535b45bf4300d37b2633c92e34ca4769
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-03-31 11:06:58 -0700

    Adding buildout, modifying install
    * Adding a buildout script to run easier in various environments
    * Modifying install to use optparse (compatible w/ 2.6 and 2.7)

--- sprinter/install.py
+++ sprinter/install.py
@@ -1,12 +1,12 @@
 """
 The install script for a sprinter-based setup script.
 """
-import argparse
 import logging
 import os
-import shutil
 import signal
 import sys
+from optparse import OptionParser
+
 from sprinter import lib
 from sprinter.environment import Environment
 
@@ -15,19 +15,20 @@ description = \
 Install an environment as specified in a sprinter config file
 """
 
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument('command', metavar='C',
-                    help="The operation that sprinter should run (install, deactivate, activate, switch)")
-parser.add_argument('target', metavar='T', help="The path to the manifest file to install", nargs='?')
-parser.add_argument('--namespace', dest='namespace', default=None,
+parser = OptionParser(description=description)
+# parser = argparse.ArgumentParser(description=description)
+# parser.add_option('command', metavar='C',
+#                     help="The operation that sprinter should run (install, deactivate, activate, switch)")
+# parser.add_option('target', metavar='T', help="The path to the manifest file to install", nargs='?')
+parser.add_option('--namespace', dest='namespace', default=None,
                     help="Namespace to check environment against")
-parser.add_argument('--username', dest='username', default=None,
+parser.add_option('--username', dest='username', default=None,
                     help="Username if the url requires authentication")
-parser.add_argument('--auth', dest='auth', action='store_true',
+parser.add_option('--auth', dest='auth', action='store_true',
                     help="Specifies authentication is required")
-parser.add_argument('--password', dest='password', default=None,
+parser.add_option('--password', dest='password', default=None,
                     help="Password if the url requires authentication")
-parser.add_argument('-v', dest='verbose', action='store_true', help="Make output verbose")
+parser.add_option('-v', dest='verbose', action='store_true', help="Make output verbose")
 
 
 def signal_handler(signal, frame):
@@ -40,33 +41,37 @@ def main(args):
     parse_args(args)
 
 
-def parse_args(args, Environment=Environment):
-    args = parser.parse_args(args)
-    command = args.command.lower()
-    logging_level = logging.DEBUG if args.verbose else logging.INFO
+def parse_args(argv, Environment=Environment):
+    options, args = parser.parse_args(argv)
+    command = args[0].lower()
+    target = args[1]
+    logging_level = logging.DEBUG if options.verbose else logging.INFO
     e = Environment(logging_level=logging_level)
     if command == "install":
-        if args.username or args.auth:
-            if not args.username:
-                args.username = lib.prompt("Please enter the username for the sprinter url...")
-            if not args.password:
-                args.password = lib.prompt("Please enter the password for the sprinter url...", secret=True)
-        e.install(args.target, namespace=args.namespace, username=args.username, password=args.password)
+        if options.username or options.auth:
+            if not options.username:
+                options.username = lib.prompt("Please enter the username for the sprinter url...")
+            if not options.password:
+                options.password = lib.prompt("Please enter the password for the sprinter url...", secret=True)
+        e.install(target,
+                  namespace=options.namespace,
+                  username=options.username,
+                  password=options.password)
     elif command == "update":
-        if args.username or args.auth:
-            if not args.username:
-                args.username = lib.prompt("Please enter the username for the sprinter url...")
-            if not args.password:
-                args.password = lib.prompt("Please enter the password for the sprinter url...", secret=True)
-        e.update(args.target, username=args.username, password=args.password)
+        if options.username or options.auth:
+            if not options.username:
+                options.username = lib.prompt("Please enter the username for the sprinter url...")
+            if not options.password:
+                options.password = lib.prompt("Please enter the password for the sprinter url...", secret=True)
+        e.update(target, username=options.username, password=options.password)
     elif command == "remove":
-        e.remove(args.target)
+        e.remove(target)
     elif command == "deactivate":
-        e.deactivate(args.target)
+        e.deactivate(target)
     elif command == "activate":
-        e.activate(args.target)
+        e.activate(target)
     elif command == "reload":
-        e.reload(args.target)
+        e.reload(target)
     elif command == "environments":
         SPRINTER_ROOT = os.path.expanduser(os.path.join("~", ".sprinter"))
         for env in os.listdir(SPRINTER_ROOT):
