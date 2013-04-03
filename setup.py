3df116af113e7c52e8d066f9a5cb7549676fb939
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-04-03 01:13:34 -0700

    rev to 0.3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
     from distutils.core import setup
 
 setup(name='sprinter',
-      version='0.3.6',
+      version='0.3.7',
       description='a utility library to help environment bootstrapping scripts',
       author='Yusuke Tsutsumi',
       author_email='yusuke@yusuketsutsumi.com',
