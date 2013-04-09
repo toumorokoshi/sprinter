2651b6c33ca9c41c9b47a92e3a82f4fe2bc513de
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-04-09 14:52:37 -0700

    Fixes #5
    * Not a good enough testing framework to verify this, but the recipestandard will no longer create the directory. Must be explicitly done by the recipe.

--- sprinter/recipestandard.py
+++ sprinter/recipestandard.py
@@ -23,27 +23,25 @@ class RecipeStandard(RecipeBase):
         """ Setup performs the setup required, with the config
         specified """
         install_directory = self.directory.install_directory(feature_name)
-        if not os.path.exists(install_directory):
-            os.makedirs(install_directory)
+        cwd = install_directory if os.path.exists(install_directory) else None
         if 'rc' in config:
             self.directory.add_to_rc(config['rc'])
         if 'command' in config:
             self.logger.info(lib.call(config['command'],
                                       bash=True,
-                                      cwd=install_directory))
+                                      cwd=cwd))
 
     def update(self, feature_name, config):
         """ Setup performs the setup required, and works with the old
         config is destruction is required """
         install_directory = self.directory.install_directory(feature_name)
-        if not os.path.exists(install_directory):
-            os.makedirs(install_directory)
+        cwd = install_directory if os.path.exists(install_directory) else None
         if 'rc' in config['target']:
             self.directory.add_to_rc(config['target']['rc'])
         if 'command' in config['target']:
             self.logger.info(lib.call(config['target']['command'],
                                       bash=True,
-                                      cwd=install_directory))
+                                      cwd=cwd))
 
     def destroy(self, feature_name, config):
         """ Destroys an old feature if it is no longer required """
