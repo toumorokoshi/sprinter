6f2666bcee4f307f510ceeb4d2dbbd03e607dd3c
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-04-15 19:15:09 -0700

    Feature: adding installing packages on recipe change in update

--- sprinter/recipes/package.py
+++ sprinter/recipes/package.py
@@ -6,6 +6,7 @@ recipe = sprinter.recipes.package
 debian = git
 brew = git
 """
+import os
 
 from sprinter.recipestandard import RecipeStandard
 from sprinter import lib
@@ -17,8 +18,16 @@ class PackageRecipe(RecipeStandard):
         super(PackageRecipe, self).__init__(environment)
         self.package_manager, self.manager_installed, self.sudo_required, self.args = self.__get_package_manager()
 
-
     def setup(self, feature_name, config):
+        self.__install_package(feature_name, config)
+        super(PackageRecipe, self).setup(feature_name, config)
+
+    def update(self, feature_name, config):
+        if config['source']['recipe'] != config['target']['recipe']:
+            self.__install_package(feature_name, config['target'])
+        super(PackageRecipe, self).update(feature_name, config)
+
+    def __install_package(self, feature_name, config):
         if self.package_manager in config:
             if not self.manager_installed:
                 if self.package_manager == "brew":
@@ -32,7 +41,6 @@ class PackageRecipe(RecipeStandard):
                 call_command = "sudo " + call_command
             self.logger.debug("Calling command: %s" % call_command)
             lib.call(call_command)
-        super(PackageRecipe, self).setup(feature_name, config)
 
     def __get_package_manager(self):
         """
@@ -49,16 +57,16 @@ class PackageRecipe(RecipeStandard):
             args = "-y"
         elif self.system.isFedoraBased():
             package = "yum"
-        installed =  lib.which(package) is not None
+        installed = lib.which(package) is not None
         if not installed:
             self.logger.error("package manager %s is not installed!" % package)
         return package, installed, sudo_required, args
 
-    def __install_brew():
+    def __install_brew(self):
         """
         install brew if possible
         """
-        if lib.which(package) is None:
+        if lib.which("brew") is None:
             if not os.path.exists('/usr/bin/xcodebuild'):
                 self.logger.error("Unable to install brew! Please install xcode command line tools:")
                 self.logger.error("https://developer.apple.com/xcode/")
