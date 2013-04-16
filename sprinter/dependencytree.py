a9ab5e9eaf64e73f99bd9e20aa7123f327569759
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-04-15 19:09:19 -0700

    Fixing bad commit with dependency tree

--- sprinter/dependencytree.py
+++ sprinter/dependencytree.py
@@ -20,31 +20,31 @@ class DependencyTree(object):
     def __init__(self, node_dict):
         self.order = self.__calculate_order(node_dict)
 
-	def __calculate_order(self, node_dict):	
-            """
-            Determine a valid ordering of the nodes in which a node is not called before all of it's dependencies.
+    def __calculate_order(self, node_dict):
+        """
+        Determine a valid ordering of the nodes in which a node is not called before all of it's dependencies.
 
-            Raise an error if there is a cycle, or nodes are missing.
-            """
-            if len(node_dict.keys()) != len(set(node_dict.keys())):
-                raise DependencyTreeException("Duplicate Keys Exist in node dictionary!")
-		valid_order = [node for node, dependencies in node_dict.items() if len(dependencies) == 0]
-		remaining_nodes = [node for node in node_dict.keys() if node not in valid_order]
-		while len(remaining_nodes) > 0:
-                    node_added = False
-                    for node in remaining_nodes:
-                        dependencies = [d for d in node_dict[node] if d not in valid_order]
-                        if len(dependencies) == 0:
-                            valid_order.append(node)
-                            remaining_nodes.remove(node)
-                            node_added = True
-                            if not node_added:
-				# the tree must be invalid, as it was not possible to remove a node.
-				# it's hard to find all the errors, so just spit out the first one you can find.
-				invalid_node = remaining_nodes[0]
-				invalid_dependency = node_dict[invalid_node][0]
-				if invalid_dependency not in remaining_nodes:
-                                    raise DependencyTreeException("The dependency list for %s is missing" % invalid_dependency)
-				else:
-                                    raise DependencyTreeException("The dependency %s is cyclic or dependent on a cyclic dependency" % invalid_dependency)
-                                return valid_order
+        Raise an error if there is a cycle, or nodes are missing.
+        """
+        if len(node_dict.keys()) != len(set(node_dict.keys())):
+            raise DependencyTreeException("Duplicate Keys Exist in node dictionary!")
+        valid_order = [node for node, dependencies in node_dict.items() if len(dependencies) == 0]
+        remaining_nodes = [node for node in node_dict.keys() if node not in valid_order]
+        while len(remaining_nodes) > 0:
+            node_added = False
+            for node in remaining_nodes:
+                dependencies = [d for d in node_dict[node] if d not in valid_order]
+                if len(dependencies) == 0:
+                    valid_order.append(node)
+                    remaining_nodes.remove(node)
+                    node_added = True
+            if not node_added:
+                # the tree must be invalid, as it was not possible to remove a node.
+                # it's hard to find all the errors, so just spit out the first one you can find.
+                invalid_node = remaining_nodes[0]
+                invalid_dependency = node_dict[invalid_node][0]
+                if invalid_dependency not in remaining_nodes:
+                    raise DependencyTreeException("The dependency list for %s is missing" % invalid_dependency)
+                else:
+                    raise DependencyTreeException("The dependency %s is cyclic or dependent on a cyclic dependency" % invalid_dependency)
+        return valid_order
--- tests/test_dependencytree.py
+++ tests/test_dependencytree.py
@@ -1,12 +1,9 @@
-import os
-import shutil
-import tempfile
 import unittest
 
 from sprinter.dependencytree import DependencyTree, DependencyTreeException
 
 LEGAL_TREE = {
-    'a': ['b','c', 'd'],
+    'a': ['b', 'c', 'd'],
     'd': [],
     'c': [],
     'b': ['d'],
@@ -14,18 +11,19 @@ LEGAL_TREE = {
 }
 
 MISSING_ENTRY_TREE = {
-    'a': ['b','c','d'],
+    'a': ['b', 'c', 'd'],
     'b': []
 }
 
 CYCLIC_TREE = {
-    'a': ['b','c','d'],
+    'a': ['b', 'c', 'd'],
     'b': [],
     'c': ['b'],
     'd': ['a']
 }
 
 LEGAL_ORDER = []
+
 
 class TestInjections(unittest.TestCase):
 
@@ -38,25 +36,25 @@ class TestInjections(unittest.TestCase):
     def test_proper_tree(self):
         """ Test whether a proper dependency tree generated the correct output. """
         dt = DependencyTree(LEGAL_TREE)
-        order = dt.order 
+        order = dt.order
         added_elements = []
         for el in order:
             for dependency in LEGAL_TREE[el]:
-                self.assertTrue(dependency in added_elements, 
-                    "Element %s depends on %s and not added yet with order of %s!" % \
-                    (el, dependency, added_elements))
+                self.assertTrue(dependency in added_elements,
+                                "Element %s depends on %s and not added yet with order of %s!" %
+                                (el, dependency, added_elements))
             added_elements.append(el)
 
     def test_missing_entry_tree(self):
-        try: 
-             DependencyTree(MISSING_ENTRY_TREE)
+        try:
+            DependencyTree(MISSING_ENTRY_TREE)
         except DependencyTreeException:
             return
         raise("Missing entry tree did not raise an error!")
 
     def test_cyclic_tree(self):
-        try: 
-             DependencyTree(CYCLIC_TREE)
+        try:
+            DependencyTree(CYCLIC_TREE)
         except DependencyTreeException:
             return
         raise("Cyclic tree did not raise an error!")
