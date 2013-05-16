65be5f365a547c9ef3e4a1672242993754a2128c
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-05-15 23:50:56 -0700

    Started on utilizing distribute to install eggs

--- sprinter/formulas/egg.py
+++ sprinter/formulas/egg.py
@@ -1,3 +1,22 @@
 """
 The egg formula will install scripts from an egg (including dependencies) into a sandboxed directory.
+[eggs]
+formula = sprinter.formulas.egg
+egg = sprinter
 """
+
+from setuptools.command.easy_install import main as easy_install
+from sprinter.formulastandard import FormulaStandard
+
+
+class EggFormula(FormulaStandard):
+
+    def __init__(self, environment):
+        super(EggFormula, self).__init__(environment)
+
+    def setup(self, feature_name, config):
+        easy_install([config['egg'], "--prefix", self.directory.root_path])
+        self.__install_package(feature_name, config)
+
+    def update(self, feature_name, source_config, target_config):
+        super(EggFormula, self).update(feature_name, source_config, target_config)
