b0a0c63de8d4c4d48b9bf0f26e0ef6275a86a4b4
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-05-25 00:49:02 -0700

    Sandboxed sprinter works
    * Sprinter config can now work standalone
    * Get basic implementation of sprinter egg

--- sprinter/formulas/egg.py
+++ sprinter/formulas/egg.py
@@ -7,6 +7,7 @@ egg = sprinter
 
 from setuptools.command.easy_install import main as easy_install
 from sprinter.formulastandard import FormulaStandard
+from sprinter.lib import call
 
 
 class EggFormula(FormulaStandard):
@@ -16,7 +17,7 @@ class EggFormula(FormulaStandard):
 
     def setup(self, feature_name, config):
         call("easy_install %s" % config['egg'])
-        self.__install_package(feature_name, config)
+        super(EggFormula, self).setup(feature_name, config)
 
     def update(self, feature_name, source_config, target_config):
         super(EggFormula, self).update(feature_name, source_config, target_config)
