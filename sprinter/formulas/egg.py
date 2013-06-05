1afd815c704ff32d29ce27d3ac347c1f50adf72b
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-06-04 19:16:10 -0700

    Re-adding egg formula

---
+++ sprinter/formulas/egg.py
@@ -0,0 +1,18 @@
+"""
+The egg formula will install scripts from an egg (including dependencies) into a sandboxed directory.
+[eggs]
+formula = sprinter.formulas.egg
+egg = sprinter
+"""
+
+from sprinter.formulabase import FormulaBase
+
+
+class EggFormula(FormulaBase):
+
+    def install(self, feature_name, config):
+        self.lib.call("easy_install %s" % config['egg'])
+        super(EggFormula, self).install(feature_name, config)
+
+    def update(self, feature_name, source_config, target_config):
+        super(EggFormula, self).update(feature_name, source_config, target_config)
