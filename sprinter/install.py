b7b6f398b8496d81b3644a643150bfeead25ea36
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-06-18 17:01:00 -0700

    PEP8 styling in install

--- sprinter/install.py
+++ sprinter/install.py
@@ -27,24 +27,24 @@ parser = OptionParser(description=description)
 #                     help="The operation that sprinter should run (install, deactivate, activate, switch)")
 # parser.add_option('target', metavar='T', help="The path to the manifest file to install", nargs='?')
 parser.add_option('--namespace', dest='namespace', default=None,
-                    help="Namespace to check environment against")
+                  help="Namespace to check environment against")
 parser.add_option('--username', dest='username', default=None,
-                    help="Username if the url requires authentication")
+                  help="Username if the url requires authentication")
 parser.add_option('--auth', dest='auth', action='store_true',
-                    help="Specifies authentication is required")
+                  help="Specifies authentication is required")
 parser.add_option('--password', dest='password', default=None,
-                    help="Password if the url requires authentication")
+                  help="Password if the url requires authentication")
 parser.add_option('-v', dest='verbose', action='store_true', help="Make output verbose")
 # not implemented yet
 parser.add_option('--sandboxbrew', dest='sandbox_brew', default=False,
-                    help="if true, sandbox a brew installation, alternatively, " + \
-                         "false will disable brew sandboxes for configuration that request it.")
+                  help="if true, sandbox a brew installation, alternatively, " +
+                  "false will disable brew sandboxes for configuration that request it.")
 parser.add_option('--sandboxaptget', dest='sandbox_aptget', default=False,
-                    help="if true, sandbox an apt-get installation, alternatively, " +
-                         "false will disable apt-get sandboxes for configuration that request it.")
+                  help="if true, sandbox an apt-get installation, alternatively, " +
+                  "false will disable apt-get sandboxes for configuration that request it.")
 parser.add_option('--virtualenv', dest='virtualenv', default=False,
-                    help="if true, will virtualenv sprinter and install eggs relative to it, " +
-                         "false will disable apt-get sandboxes for configuration that request it.")
+                  help="if true, will virtualenv sprinter and install eggs relative to it, " +
+                  "false will disable apt-get sandboxes for configuration that request it.")
 
 
 def signal_handler(signal, frame):
