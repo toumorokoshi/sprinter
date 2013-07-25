5e5490c455322f574fefb4ec6f469cdf21916010
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-07-25 15:14:13 -0700

    Adding directory for brew

--- sprinter/brew.py
+++ sprinter/brew.py
@@ -1,6 +1,7 @@
 """
 Utility methods for brew
 """
+import os
 from sprinter.lib import extract_targz
 
 HOMEBREW_URL = "https://github.com/mxcl/homebrew/tarball/master"
@@ -8,4 +9,6 @@ HOMEBREW_URL = "https://github.com/mxcl/homebrew/tarball/master"
 
 def install_brew(target_path):
     """ Install brew to the target path """
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
     extract_targz(HOMEBREW_URL, target_path, remove_common_prefix=True)
