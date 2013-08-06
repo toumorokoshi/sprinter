8e117d4f598bf23e86273381cfcef739ee0fe547
Daniel Stockman <daniels@zillow.com>
2013-08-06 11:34:36 -0700

    Make env formula's exclusion of keys more graceful.

--- sprinter/formula/env.py
+++ sprinter/formula/env.py
@@ -14,13 +14,16 @@ from sprinter.formulabase import FormulaBase
 class EnvFormula(FormulaBase):
     """ A sprinter formula for git"""
 
+    # the keys that should be ignored during write loop (anything that has meaning elsewhere)
+    ignored_keys = FormulaBase.valid_options + FormulaBase.required_options
+
     def install(self):
-        for c in (c for c in self.target.keys() if c != 'formula' && c != 'depends'):
+        for c in (c for c in self.target.keys() if c not in self.ignored_keys):
             self.directory.add_to_env('export %s=%s' % (c.upper(), self.target.get(c)))
         FormulaBase.install(self)
 
     def update(self):
-        for c in (c for c in self.target.keys() if c != 'formula' && c != 'depends'):
+        for c in (c for c in self.target.keys() if c not in self.ignored_keys):
             self.directory.add_to_env('export %s=%s' % (c.upper(), self.target.get(c)))
         FormulaBase.update(self)
 
