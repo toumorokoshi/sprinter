f518960b58f3fbceb528f12aa4f676236a26bc79
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-11-26 23:49:36 -0800

    revving to 1.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
     from distutils.core import setup
 
 setup(name='sprinter',
-      version='1.1.3',
+      version='1.1.4',
       description='a utility library to help environment bootstrapping scripts',
       long_description=open('README.rst').read(),
       author='Yusuke Tsutsumi',
