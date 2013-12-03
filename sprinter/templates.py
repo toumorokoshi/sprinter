9e6e36e7912f2bbedee6e9eb6b7db74ed9267a65
Daniel Stockman <daniels@zillow.com>
2013-12-03 15:08:34 -0800

    Add __sprinter_remove_path function to utils.sh.

--- sprinter/templates.py
+++ sprinter/templates.py
@@ -18,6 +18,15 @@ __sprinter_prepend_path() {
         export $sp_var="${sp_dir}${sp_list:+":$sp_list"}"
     fi
 }
+
+# remove a path from env var (default PATH)
+__sprinter_remove_path() {
+    local sp_dir="$1"
+    local sp_var="${2:-PATH}"
+    local sp_list=$(eval echo '$'$sp_var)
+    # strip sp_dir from sp_list
+    export $sp_var=$(echo -n $sp_list | awk -v RS=: -v ORS=: '$0 != "'$sp_dir'"' | sed -E 's/^:|:$//g')
+}
 """
 
 source_template = """[ -r "%s" ] && . %s\n"""
