c224335f67ebaa0543452ce120cd60ac90c9d9ac
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-01-08 08:21:08 +0700

    Created more complex sprinter sandbox script, removes previous sandboxes now

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -1,8 +1,16 @@
 # a one-liner to install a sandboxed sprinter. Use this if you do not
 # want to or can not install sprinter as sudo.
 # this is also a great example of creating a standalone installer for an environment
+error () {
+    last_error=$?
+    echo $1;
+    exit $last_error
+}
+
+echo "Creating sandbox directory..."
 mkdir -p /tmp/sprinter-sandbox
 cd /tmp/sprinter-sandbox
+echo "Creating python sandbox..."
 # install virtualenv
 if [[ `uname` == 'Linux' ]]; then
     wget https://raw.github.com/toumorokoshi/sprinter/master/sprinter/external/virtualenv.py
@@ -11,11 +19,21 @@ elif [[ `uname` == 'Darwin' ]]; then
 fi
 python virtualenv.py sprinter-dir
 cd sprinter-dir
+echo "Installing sprinter to sandbox..."
 bin/easy_install http://github.com/toumorokoshi/sprinter/tarball/master
  
 # Put your Commands here. e.g.:
+echo "Removing sprinter environment if it already exists..."
+bin/sprinter remove sprinter
+if [[ -d ~/.sprinter/sprinter/ ]]; then
+    echo "Sprinter environment was not removed cleanly. Forcefully removing..."
+    rm -rf ~/.sprinter/sprinter/
+fi
+echo "Installing global sprinter..."
 bin/sprinter install https://raw.github.com/toumorokoshi/sprinter/master/examples/sprinter.cfg
  
  
 # finally, delete the temporary directory
+echo "Cleaning up..."
 rm -r /tmp/sprinter-sandbox
+echo "Done!"
