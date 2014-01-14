a0b17ba4a12e90108a2ce0ab7b1795d85af3c6d0
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-01-13 20:46:15 -0800

    Making sandbox with proper sandboxing

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -3,14 +3,15 @@
 # this is also a great example of creating a standalone installer for an environment
 error () {
     last_error=$?
-    echo $1;
+    echo "!!"
+    echo $1
     exit $last_error
 }
 
 echo "Creating sandbox directory..."
 mkdir -p /tmp/sprinter-sandbox
 cd /tmp/sprinter-sandbox
-echo "Creating python sandbox..."
+echo "Downlading sprinter..."
 # install virtualenv
 if [[ `uname` == 'Linux' ]]; then
     wget https://raw.github.com/toumorokoshi/sprinter/master/sprinter/external/virtualenv.py
@@ -18,15 +19,16 @@ elif [[ `uname` == 'Darwin' ]]; then
     curl -o sprinter.tar.gz http://github.com/toumorokoshi/sprinter/tarball/master -L
 fi
 tar -xzvf sprinter.tar.gz --strip-components=1 &> /dev/null || error "Failure extracting sprinter targz!"
-python bootstrap.py || error "Failure with bootstrap.py!"
+echo "Creating python sandbox..."
+python bootstrap.py &> /dev/null || error "Failure with bootstrap.py!"
 echo "Installing sprinter to sandbox..."
 bin/buildout -c buildout-install.cfg &> /dev/null || error "Failure with buildout!"
  
 echo "Removing sprinter environment if it already exists..."
-bin/sprinter remove sprinter || error "Failure removing sprinter!"
+bin/sprinter remove sprinter
 if [[ -d ~/.sprinter/sprinter/ ]]; then
     echo "Sprinter environment was not removed cleanly. Forcefully removing..."
-    rm -rf ~/.sprinter/sprinter/ || error "Failure forcefully removing sprinter!"
+    rm -rf ~/.sprinter/sprinter/
 fi
 echo "Installing global sprinter..."
 bin/sprinter install https://raw.github.com/toumorokoshi/sprinter/master/examples/sprinter.cfg || error "Issue installing global sprinter!"
