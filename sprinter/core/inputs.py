8583ad0857a525fee794b84901360da450cf6930
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-01-25 00:22:50 -0800

    allowing custom prompts for inputs

--- sprinter/core/inputs.py
+++ sprinter/core/inputs.py
@@ -1,31 +1,42 @@
 from __future__ import unicode_literals
 from sprinter import lib
 
-INPUT_EMPTY_VALUE = object()
+EMPTY = object()
 
 
 class InputException(Exception):
     pass
 
 
+class Input(object):
+    """ struct to hold input information """
+
+    value = EMPTY
+    default = EMPTY
+    is_secret = False
+    prompt = None
+
+    def __eq__(self, other):
+        for val in ('value', 'default', 'is_secret', 'prompt'):
+            a = getattr(self, val, None)
+            b = getattr(other, val, None)
+            if not (a == b or a is b):
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
 class Inputs(object):
     """" A class to handle user input """
-    
-    def __init__(self):
-        self._secret_values = set()  # secret values are not written on save
-        self._inputs = set()  # a list of valid inputs to retrieve values from
-        self._defaults = {}  # a dictionary of defaults to use
-        self._values = {}  # dictionary to store values in
 
-    def add_input(self, key, value=INPUT_EMPTY_VALUE, default=INPUT_EMPTY_VALUE, is_secret=False):
+    def __init__(self):
+        self._inputs = {}
+
+    def add_input(self, key, input_instance=None):
         """ Add an input <input> with a possible <value>, and <is_secret>"""
-        self._inputs.add(key)
-        if is_secret:
-            self._secret_values.add(key)
-        if value is not INPUT_EMPTY_VALUE:
-            self._values[key] = value
-        if default is not INPUT_EMPTY_VALUE:
-            self._defaults[key] = default
+        self._inputs[key] = input_instance or Input()
 
     def is_input(self, key):
         """ Returns true if <key> is a key """
@@ -34,27 +45,40 @@ class Inputs(object):
     def is_set(self, key):
         if key not in self._inputs:
             raise InputException("Key {0} is not a valid input!".format(key))
-        return key in self._values
+        return self._inputs[key].value is not EMPTY
 
     def set_input(self, key, value):
         """ Sets the <key> to <value> """
         if key not in self._inputs:
             raise InputException("Key {0} is not a valid input!".format(key))
-        self._values[key] = value
+        self._inputs[key].value = value
 
     def get_input(self, key, force=False):
         """ Get the value of <key> if it already exists, or prompt for it if not """
+        prompt = "please enter your {0}".format(key)
+        if self._inputs[key].prompt:
+            prompt = self._inputs[key].prompt
         if key not in self._inputs:
             raise InputException("Key {0} is not a valid input!".format(key))
-        if key not in self._values or force:
-            self._values[key] = lib.prompt("please enter your %s" % key,
-                                           default=(self._values.get(key, None) or self._defaults.get(key, None)),
-                                           secret=(key in self._secret_values))
-        return self._values[key]
+
+        if self._inputs[key].value is EMPTY or force:
+
+            default_value = None
+            if self._inputs[key].default is not EMPTY:
+                default_value = self._inputs[key].default
+            if self._inputs[key].value is not EMPTY:
+                default_value = self._inputs[key].value
+
+            self._inputs[key].value = lib.prompt(
+                prompt,
+                default=default_value,
+                secret=self._inputs[key].is_secret)
+
+        return self._inputs[key].value
 
     def get_unset_inputs(self):
         """ Return a set of unset inputs """
-        return set([k for k in self._inputs if k not in self._values])
+        return set([k for k, v in self._inputs.items() if v.value is EMPTY])
 
     def prompt_unset_inputs(self, force=False):
         """ Prompt for unset input values """
@@ -67,21 +91,21 @@ class Inputs(object):
 
     def keys(self):
         """ Return a set of valid keys """
-        return self._inputs
+        return self._inputs.keys()
 
     def values(self, with_defaults=True):
         """ Return the values dictionary, defaulting to default values """
         return_dict = {}
-        for k in self._inputs:
-            if k in self._values:
-                return_dict[k] = self._values[k]
-            elif k in self._defaults:
-                return_dict[k] = self._defaults[k]
+        for k, v in self._inputs.items():
+            if v.value is not EMPTY:
+                return_dict[k] = v.value
+            elif with_defaults and v.default is not EMPTY:
+                return_dict[k] = v.default
         return return_dict
 
     def write_values(self):
         """ Return the dictionary with which to write values """
-        return dict(((k, v) for k, v in self.values().items() if k not in self._secret_values))
+        return dict(((k, v) for k, v in self.values().items() if not self._inputs[k].is_secret))
 
     def add_inputs_from_inputstring(self, input_string):
         """
@@ -95,20 +119,28 @@ class Inputs(object):
         raw_params = input_string.split('\n')
         param_attributes = (self._parse_param_line(rp) for rp in raw_params if len(rp.strip(' \t')) > 0)
         for param, attributes in param_attributes:
-            self.add_input(param,
-                           default=attributes.get('default', INPUT_EMPTY_VALUE),
-                           is_secret=attributes.get('secret', False))
+            self.add_input(param, attributes)
 
     def _parse_param_line(self, line):
         """ Parse a single param line. """
         value = line.strip('\n \t')
         if len(value) > 0:
-            attribute_dict = {}
+            i = Input()
+            if value.find('#') != -1:
+                value, extra_attributes = value.split('#')
+                try:
+                    extra_attributes = eval(extra_attributes)
+                except SyntaxError:
+                    raise InputException("Incorrectly formatted input for {0}!".format(value))
+                if not isinstance(extra_attributes, dict):
+                    raise InputException("Incorrectly formatted input for {0}!".format(value))
+                if 'prompt' in extra_attributes:
+                    i.prompt = extra_attributes['prompt']
             if value.find('==') != -1:
-                value, default = line.split('==')
-                attribute_dict['default'] = default
+                value, default = value.split('==')
+                i.default = default
             if value.endswith('?'):
                 value = value[:-1]
-                attribute_dict['secret'] = True
-            return (value, attribute_dict)
+                i.is_secret = True
+            return (value, i)
         return None
--- sprinter/core/tests/test_inputs.py
+++ sprinter/core/tests/test_inputs.py
@@ -1,7 +1,19 @@
 from nose import tools
 from mock import patch, call
-from sprinter.core.inputs import Inputs
+from sprinter.core.inputs import Inputs, Input, InputException
 from sprinter import lib
+
+
+class TestInput(object):
+
+    def test_input_equality(self):
+        a = Input()
+        a.value = 'hi'
+        a.is_secret = True
+        b = Input()
+        b.value = 'hi'
+        b.is_secret = True
+        assert a == b
 
 
 class TestInputs(object):
@@ -15,7 +27,9 @@ class TestInputs(object):
     def test_prompt_unset_imputs(self):
         """ Only unset inputs should be prompted with prompt_unset_inputs """
         self.inputs.add_input('key')
-        self.inputs.add_input('key_with_value', 'value')
+        key_with_value = Input()
+        key_with_value.value = 'value'
+        self.inputs.add_input('key_with_value', key_with_value)
         with patch.object(lib, 'prompt') as prompt:
             prompt.return_value = "yusuke"
             self.inputs.prompt_unset_inputs()
@@ -24,7 +38,9 @@ class TestInputs(object):
     def test_forced_prompt_unset_imputs(self):
         """ Only all inputs should be prompted with prompt_unset_inputs and force=True """
         self.inputs.add_input('key')
-        self.inputs.add_input('key_with_value', 'value')
+        key_with_value = Input()
+        key_with_value.value = 'value'
+        self.inputs.add_input('key_with_value', key_with_value)
         with patch.object(lib, 'prompt') as prompt:
             prompt.return_value = "yusuke"
             self.inputs.prompt_unset_inputs(force=True)
@@ -35,8 +51,30 @@ class TestInputs(object):
 
     def test_write_values(self):
         """ Only write values should be returned by write_values """
-        self.inputs.add_input('secret_key', 'value', is_secret=True)
-        self.inputs.add_input('not_secret_key', 'not_secret_value')
+        secret = Input()
+        secret.value = 'value'
+        secret.is_secret = True
+        not_secret = Input()
+        not_secret.value = 'not_secret_value'
+        self.inputs.add_input('secret_key', secret)
+        self.inputs.add_input('not_secret_key', not_secret)
         tools.eq_(self.inputs.write_values(), {
             'not_secret_key': 'not_secret_value'
         })
+
+    def test_parse_param_line_with_dict(self):
+        """ parse_param_line should accept a dict to configure itself """
+        test_line = "test?==result#{'prompt':'would you like to do something?'}"
+        value, config = self.inputs._parse_param_line(test_line)
+        assert value == 'test'
+        test_config = Input()
+        test_config.prompt = 'would you like to do something?'
+        test_config.is_secret = True
+        test_config.default = 'result'
+        assert config == test_config
+
+    @tools.raises(InputException)
+    def test_parse_param_line_with_bad_dict(self):
+        """ a bad param line should raise an input exception """
+        test_line = "test==result?#{'prompt':'would you like to do something?"
+        value, attribute_dict = self.inputs._parse_param_line(test_line)
