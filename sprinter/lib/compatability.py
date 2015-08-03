9fa4a8fa845e2f42d9673b0e540b8c1f40997669
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-08-03 09:00:08 -0700

    making the unicode converstion py3 compatible.

--- sprinter/lib/compatability.py
+++ sprinter/lib/compatability.py
@@ -4,8 +4,20 @@ A set of compatibility methods
 import sys
 from six.moves import configparser
 
+
 def create_configparser():
     if sys.version_info > (3, 0):
         return configparser.RawConfigParser(strict=True)
     else:
         return configparser.RawConfigParser()
+
+
+if sys.version_info > (3, 0):
+    def _unicode(s):
+        return s
+else:
+    def _unicode(s):
+        """ return the string converted to unicode. """
+        if not isinstance(s, unicode):
+            s = s.decode("utf8")
+        return s
