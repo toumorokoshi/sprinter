6ade4f555f80e91cf206851fe9e4e7708fe02a2b
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2015-10-31 19:50:31 -0700

    started on some more factoring in sprinter.

---
+++ sprinter/globals.py
@@ -0,0 +1,14 @@
+import yaml
+
+
+def get_global(path):
+    global_config_path = os.path.join(path, ".global")
+    yaml.load(
+
+
+class Globals(object):
+
+    def __init__(self, global_config):
+        self._path = path
+
+    def
