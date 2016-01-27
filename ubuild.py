ea0ddaac749baaac6f91a210cebfa610da612b9a
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-01-26 20:15:25 -0800

    fixing exit codes.

--- ubuild.py
+++ ubuild.py
@@ -1,9 +1,5 @@
-import sys
-
-
 def main(build):
     build.packages.install(".", develop=True)
-    build.packages.install("pex")
 
 
 def test(build):
@@ -12,6 +8,6 @@ def test(build):
     build.packages.install("mock")
     build.packages.install("pytest")
     build.packages.install("nose")
-    sys.exit(build.executables.run(
+    return build.executables.run(
         ["py.test", "sprinter"] + build.options.args
-    )[0])
+    )[0]
