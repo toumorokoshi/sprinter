d65ce3b50bfb985e5c5595fe225de2569344b420
Jonathan Felchlin <jonathan@xgecko.com>
2016-02-16 15:31:38 -0800

    Added `nvm use {version}` call, when node_version is passed

--- sprinter/formula/npm.py
+++ sprinter/formula/npm.py
@@ -34,18 +34,30 @@ class NPMFormula(FormulaBase):
     def install(self):
         npm_root = self.target.get('npm_root')
         modules_dir = os.path.join(npm_root, 'node_modules')
+
         if os.path.exists(modules_dir):
             shutil.rmtree(modules_dir)
+
         self.__run_command('install', self.target)
+
+        if self.target.has('node_version'):
+            self.directory.add_to_env(self.__nvm_use(config=self.target))
+
         FormulaBase.install(self)
 
     def update(self):
         source_npm_root = self.source.get('npm_root')
         target_npm_root = self.target.get('npm_root')
         modules_dir = os.path.join(target_npm_root, 'node_modules')
+
         if target_npm_root != source_npm_root or not os.path.exists(modules_dir):
             return self.install()
+
         self.__run_command('update', self.target)
+
+        if self.target.has('node_version'):
+            self.directory.add_to_env(self.__nvm_use(config=self.target))
+
         if self.target.is_affirmative('list_outdated'):
             self.__run_command('outdated', self.target)
 
@@ -76,17 +88,24 @@ class NPMFormula(FormulaBase):
         shutil.move(modules_dir, modules_save)
         FormulaBase.deactivate(self)
 
+    def __nvm_use(self, config):
+        if not config.has('node_version'):
+            return ''
+        return 'nvm use {version}'.format(
+            version=config.get('node_version'))
+
     def __run_command(self, command, config):
-        node_version = self.target.get('node_version')
-        npm_root = self.target.get('npm_root')
+        npm_root = config.get('npm_root')
+        shell = False
         # using depth 0, even though it's the default, to minimize npm's tree output
-        nvm_command = ''
+
         if config.has('node_version'):
-            nvm_command = 'nvm use {version} && '.format(version=config.get('node_version'))
-        full_command = "{pre} npm {cmd} --depth 0".format(pre=nvm_command, cmd=command)
+            nvm_command = '{cmd} && '.format(cmd=self.__nvm_use(config=config))
+            shell = True
+        full_command = "{pre}npm {cmd} --depth 0".format(pre=nvm_command, cmd=command)
         # self.logger.debug("Running {cmd}...".format(cmd=full_command))
         stdout = subprocess.PIPE if config.is_affirmative('redirect_stdout_to_log', 'true') else None
-        return_code, output = lib.call(full_command, shell=False, stdout=stdout, cwd=npm_root)
+        return_code, output = lib.call(full_command, shell=shell, stdout=stdout, cwd=npm_root)
         if config.is_affirmative('fail_on_error', True) and return_code != 0:
             raise NPMFormulaException("npm returned a return code of {0}!".format(return_code))
         return True
