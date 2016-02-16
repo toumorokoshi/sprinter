c372be44b3948220a042e5da04b182c6fe7e6bfb
Jonathan Felchlin <jonathan@xgecko.com>
2016-02-16 14:21:28 -0800

    Adding sprinter.formula.npm

---
+++ sprinter/formula/npm.py
@@ -0,0 +1,88 @@
+"""
+Maintains an npm package
+
+[config]
+inputs = git_root==~/code/project
+
+[ssh]
+formula = sprinter.formula.npm
+npm_root = %(config:git_root)s
+node_version = 4.3.0
+list_outdated = true
+redirect_stdout_to_log = false
+"""
+from __future__ import unicode_literals
+
+import os
+import subprocess
+import shutil
+
+from sprinter.formula.base import FormulaBase
+from sprinter.exceptions import FormulaException
+import sprinter.lib as lib
+
+
+class NPMFormulaException(FormulaException):
+    pass
+
+
+class NPMFormula(FormulaBase):
+
+    required_options = FormulaBase.required_options + ['npm_root']
+    valid_options = FormulaBase.valid_options + ['list_outdated', 'node_version']
+
+    def install(self):
+        npm_root = self.target.get('npm_root')
+        modules_dir = os.path.join(npm_root, 'node_modules')
+        if os.path.exists(modules_dir):
+            shutil.rmtree(modules_dir)
+        self.__run_command('install', self.target)
+        FormulaBase.install(self)
+
+    def update(self):
+        source_npm_root = self.source.get('npm_root')
+        target_npm_root = self.target.get('npm_root')
+        modules_dir = os.path.join(target_npm_root, 'node_modules')
+        if target_npm_root != source_npm_root or not os.path.exists(modules_dir):
+            return self.install()
+        self.__run_command('update', self.target)
+        if self.target.is_affirmative('list_outdated'):
+            self.__run_command('outdated', self.target)
+
+        return FormulaBase.update(self)
+
+    def activate(self):
+        namespace = self.target.get('namespace')
+        npm_root = self.target.get('npm_root')
+        modules_dir = os.path.join(npm_root, 'node_modules')
+        modules_restore = os.path.join(npm_root, 'node_modules.{ns}'.format(ns=namespace))
+        modules_bak = os.path.join(npm_root, 'node_modules.sv')
+
+        if os.path.exists(modules_dir):
+            if os.path.exists(modules_bak):
+                shutil.rmtree(modules_bak)
+            shutil.move(modules_dir, modules_bak)
+        shutil.move(modules_restore, modules_dir)
+        FormulaBase.activate(self)
+
+    def deactivate(self):
+        namespace = self.target.get('namespace')
+        npm_root = self.target.get('npm_root')
+        modules_dir = os.path.join(npm_root, 'node_modules')
+        modules_save = os.path.join(npm_root, 'node_modules.{ns}'.format(ns=namespace))
+
+        if os.path.exists(modules_save):
+            shutil.rmtree(modules_save)
+        shutil.move(modules_dir, modules_save)
+        FormulaBase.deactivate(self)
+
+    def __run_command(self, command, config):
+        npm_root = self.target.get('npm_root')
+        # using depth 0, even though it's the default, to minimize npm's tree output
+        full_command = "npm {cmd} --depth 0".format(cmd=command)
+        # self.logger.debug("Running {cmd}...".format(cmd=full_command))
+        stdout = subprocess.PIPE if config.is_affirmative('redirect_stdout_to_log', 'true') else None
+        return_code, output = lib.call(full_command, shell=False, stdout=stdout, cwd=npm_root)
+        if config.is_affirmative('fail_on_error', True) and return_code != 0:
+            raise NPMFormulaException("npm returned a return code of {0}!".format(return_code))
+        return True
